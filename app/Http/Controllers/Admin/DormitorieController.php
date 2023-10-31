786a2f76c9b0cc3d8c0eb470b846f9cd9b050100
Liliana2201 <subinaliliana@gmail.com>
2023-10-31 13:03:50 +0700

    разбиение на части шаблона

---
+++ app/Http/Controllers/Admin/DormitorieController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Dormitories;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DormitorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $dormitories = Dormitories::paginate(10);
+        return view('admin.dormitories.index', compact('dormitories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.dormitories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+            'url_photo' => 'nullable|text',
+        ]);
+        Dormitories::create($request->all());
+        //$request->session()->flash('success', 'Общежитие добавлено!');
+        return redirect()->route('dormitories.index')->with('success', 'Общежитие добавлено!');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $dormitory = Dormitories::find($id);
+        return view('admin.dormitories.edit', compact('dormitory'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+            'url_photo' => 'nullable|text'
+        ]);
+        $dormitory = Dormitories::find($id);
+        $dormitory->update($request->all());
+        //$request->session()->flash('success', 'Общежитие добавлено!');
+        return redirect()->route('dormitories.index', ['dormitory' => $dormitory->id_dom])->with('success', 'Общежитие изменено!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        Dormitories::destroy($id);
+        return redirect()->route('dormitories.index');
+    }
+}
--- resources/views/admin/dormitories/edit.blade.php
+++ resources/views/admin/dormitories/edit.blade.php
@@ -16,28 +16,29 @@
             <div class="card-header">
                 <h3 class="card-title">Изменить общежитие</h3>
             </div>
-            <form role="form" method="post" action="{{ route('dormitories.store') }}">
+            <form role="form" method="post" action="{{ route('dormitories.update', ['dormitory' => $dormitory->id_dom]) }}"  enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="card-body">
                     <div class="form-group">
                         <label for="title">Название</label>
-                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="Введите название">
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="Введите название" value="{{ $dormitory->title }}">
                     </div>
                     <div class="form-group">
                         <label for="address">Адрес</label>
-                        <input type="text" class="form-control  @error('address') is-invalid @enderror" id="address" placeholder="Введите адрес">
+                        <input type="text" class="form-control  @error('address') is-invalid @enderror" id="address" placeholder="Введите адрес" value="{{ $dormitory->address }}">
                     </div>
                     <div class="form-group">
                         <label for="phone">Номер вахты</label>
-                        <input type="number" class="form-control  @error('phone') is-invalid @enderror" id="phone" placeholder="Введите номер">
+                        <input type="number" class="form-control  @error('phone') is-invalid @enderror" id="phone" placeholder="Введите номер" value="{{ $dormitory->phone }}">
                     </div>
                     <div class="form-group">
                         <label for="url_photo">Добавить фото</label>
-                        <input type="text" class="form-control  @error('url_photo') is-invalid @enderror" id="url_photo" placeholder="Введите url фото">
+                        <input type="text" class="form-control  @error('url_photo') is-invalid @enderror" id="url_photo" placeholder="Введите url фото" value="{{ $dormitory->url_photo }}">
                     </div>
                 </div>
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Добавить</button>
+                    <button type="submit" class="btn btn-primary">Сохранить</button>
                 </div>
             </form>
         </div>
